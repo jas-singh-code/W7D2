a2da0f9f7b77da5aef3869bfdd8861232e02bb84
jas-singh-code <jaspreetsingh7798@gmail.com>
2021-02-09 22:39:21 -0500

    added validates specs to UserModel

--- Music_App/test/test_helper.rb
+++ Music_App/test/test_helper.rb
@@ -2,6 +2,13 @@ ENV['RAILS_ENV'] ||= 'test'
 require_relative '../config/environment'
 require 'rails/test_help'
 
+Shoulda::Matchers.configure do |config|
+  config.integrate do |with|
+    with.test_framework :rspec
+    with.library :rails
+  end
+end
+
 class ActiveSupport::TestCase
   # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
   fixtures :all
